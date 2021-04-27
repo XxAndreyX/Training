e13cd572be68d8534e299180aba8ece1ac502cdc
XxAndreyX <andrey.gubanov.95@mail.ru>
2021-04-27 13:53:31 +0300

    New loop has been added

--- JS/script.js
+++ JS/script.js
@@ -6,14 +6,23 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
+for (let i = 0; i < 2; i++){
+    const a = prompt("Last Watched film?");
+    const b = prompt("Your rating?");
+    personalMovieDB.movies[a] = b;
+    if (a != null && b != null && a != ''&& b !== '' && a.length < 50){
+        console.log("Well done!");
+    } else{
+        console.log("Error");
+        i--;
+    }
 
-const a = prompt("Last Watched film?");
-const b = prompt("Your rating?");
-const c = prompt("Last Watched film?");
-const d = prompt("Your rating?");
+}
+if (personalMovieDB.count < 10) {
+    console.log("Watched not to many films")
+} else if (personalMovieDB > 30){
+    console.log("Watched many films")
+} else console.log("Norm")
 
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB)
